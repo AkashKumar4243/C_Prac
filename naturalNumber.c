62acc8ab29172b12e83c87f9c0c728f1de7f570e
Akash Kumar <akash2002155@gmail.com>
2026-09-13 15:53:18 +0530

    while and do_while added in naturalNumber

--- naturalNumber.c
+++ naturalNumber.c
@@ -8,10 +8,28 @@ int main (){
     scanf("%d",&n);
 
     sum = 0;
-    for (int i = 0 ; i <=n ; i++){
+
+    // Using for loop
+    // for (int i = 0 ; i <=n ; i++){
+    //     printf("%d \n",i);
+    //     sum += i;
+    // }
+
+    // using while loop
+    // int i = 0;
+    // while (i<=n){
+    //     printf("%d \n",i);
+    //     sum += i;
+    //     i++;
+    // }
+
+    //using do while 
+    int i = 0;
+    do {
         printf("%d \n",i);
         sum += i;
-    }
+        i++;
+    }while (i<=n);
     
     printf("sum of n number = %d",sum);
 
